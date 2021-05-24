e930e453d19dbc369480dda50b49c7e3e783e12f
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-05-25 00:39:33 +0530

    security: basic auth implemented

---
+++ spring-basics/src/main/java/com/ani/security/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.ani.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable() // not recommended
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+}
