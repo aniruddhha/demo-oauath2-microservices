cc4bd3513be1ea446705edf2d6625c50fb580ccd
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-05-25 23:55:36 +0530

    initial commit

---
+++ central-configuration/src/test/java/com/ani/central/configuration/CentralConfigurationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.central.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralConfigurationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
