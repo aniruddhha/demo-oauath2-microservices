0e9f5e1f3345de0a438dff64cffb1ff144c989b1
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-05-25 23:54:45 +0530

    initial commit

---
+++ machine-microservice/src/main/java/com/ani/machine/MachineMicroserviceApplication.java
@@ -0,0 +1,11 @@
+package com.ani.machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MachineMicroserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MachineMicroserviceApplication.class, args);
+	}
+}
---
+++ machine-microservice/src/test/java/com/ani/machine/MachineMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ani.machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MachineMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
