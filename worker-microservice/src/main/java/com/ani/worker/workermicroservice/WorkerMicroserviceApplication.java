c1f9a52ce4bd8a620684b5a0d4ba2c2b3804f67b
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-05-26 21:49:02 +0530

    docker link

--- worker-microservice/src/main/java/com/ani/worker/workermicroservice/WorkerMicroserviceApplication.java
+++ worker-microservice/src/main/java/com/ani/worker/workermicroservice/WorkerMicroserviceApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+// docker run -d -p 8787:8080 -e KEYCLOAK_USER=admin -e KEYCLOAK_PASSWORD=admin quay.io/keycloak/keycloak:13.0.0
+
 @EnableEurekaClient
 @SpringBootApplication
 public class WorkerMicroserviceApplication {
