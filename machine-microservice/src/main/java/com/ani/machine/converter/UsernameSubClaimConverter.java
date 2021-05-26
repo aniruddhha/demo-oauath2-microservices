13319627ba7f48e38aea9de7df80ab746c0f61fd
aniruddhha <aniruddha.kudalkar@gmail.com>
2021-05-27 02:00:25 +0530

    oauth2 integration

---
+++ machine-microservice/src/main/java/com/ani/machine/converter/UsernameSubClaimConverter.java
@@ -0,0 +1,20 @@
+package com.ani.machine.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.oauth2.jwt.MappedJwtClaimSetConverter;
+
+import java.util.Collections;
+import java.util.Map;
+
+public class UsernameSubClaimConverter implements Converter< Map<String, Object>,Map<String, Object> > {
+
+    private final MappedJwtClaimSetConverter delegate = MappedJwtClaimSetConverter.withDefaults(Collections.emptyMap());
+
+    @Override
+    public Map<String, Object> convert(Map<String, Object> claims) {
+        Map<String, Object> convertedClaims = this.delegate.convert(claims);
+        String userName = (String) convertedClaims.get("preferred_username");
+        convertedClaims.put("sub", userName);
+        return convertedClaims;
+    }
+}
---
+++ worker-microservice/src/main/java/com/ani/worker/workermicroservice/security/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.ani.worker.workermicroservice.security;
+
+import com.ani.worker.workermicroservice.converter.KeycloakRealmRoleConverter;
+import com.ani.worker.workermicroservice.converter.UsernameSubClaimConverter;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(
+                        jwtAuthenticationConverter()
+                );
+
+        http.authorizeRequests().anyRequest().authenticated();
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtAuthenticationConverter converter = new JwtAuthenticationConverter();
+        converter.setJwtGrantedAuthoritiesConverter(new KeycloakRealmRoleConverter());
+        return converter;
+    }
+
+    @Bean
+    public JwtDecoder decodeByIssuerUri(OAuth2ResourceServerProperties properties) {
+        String issuerUri = properties.getJwt().getIssuerUri();
+        NimbusJwtDecoder decoder = (NimbusJwtDecoder) JwtDecoders.fromIssuerLocation(issuerUri);
+        decoder.setClaimSetConverter(new UsernameSubClaimConverter());
+        return decoder;
+    }
+}
